7cd67dbb36b82df8c52bc97ab3fc139ebdb47814
mbusenitz <mbusenitz@gmail.com>
2017-11-19 06:24:53 -0800

    tmp

--- index.js
+++ index.js
@@ -7,40 +7,44 @@ let run = require( 'runjs' ).run
   , path = require( 'path' )
   , fs = require( 'fs' )
   , mkdirp = require( 'mkdirp' )
+  , tmp = require( 'tmp' )
   , argv = require('yargs')
 	.command('install', 'install developer dependencies', {}, (yargs) => {
-		run( 'npm install --only=dev' );
 
-		fs.readdir( nodeModules, (err, paths) => {
+		mkdirp( destDir, (err) => {
+			
+			const selfRef = path.join('test/lib/', path.basename( process.cwd() ) );
+			fs.stat( selfRef, (err, stat) => {
+				if (err) {
+					run( 'ln -s ../.. ' + selfRef );
+				}
+			}); 
+			
+			highJackNodeModules();
+		});
 
-			mkdirp( destDir, (err) => {
+		function highJackNodeModules() {
+			let tmpObj = tmp.dirSync();
+			let nodeDir = path.join( tmpObj.name, nodeModules );
 
-				run( 'ln -s ../.. ' + path.join('test/lib/', path.basename(__dirname) ) );
-
+			run( 'cp package.json ' + tmpObj.name );
+			run( 'npm install --only=dev', {cwd: tmpObj.name });
+			fs.readdir( nodeDir, (err, paths) => {
+				if (err) throw err;
 				paths.forEach( (dir) => {
 					const destSubDir = path.join( destDir, dir );
 					
 					fs.stat(destSubDir, (err, stat) => {
 						if (err)
 						{
-							const sourceDir = path.join( nodeModules, dir ); 
+							const sourceDir = path.join( nodeDir, dir ); 
 							run( 'mv ' + sourceDir + ' ' + destSubDir ); 
 						}
 					});
 				}); 
-			});
-		}); 
+			}); 
+		}
 	})
 	.demandCommand()
 	.help()
 	.argv;
-
-/*
-
-
-install --dev
-cd test
-npm install
-mv node_modules lib
-ln -s ../../ lib/context
-*/ 
